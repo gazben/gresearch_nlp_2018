d522376d5ac831edd4684a3dfeef10667c511128
Gazder Bence <bencegazder@gmail.com>
2018-06-08 17:45:15 +0200

    Initial commit

---
+++ __sample/rpnevaluator.py
@@ -0,0 +1,8 @@
+class RpnEvaluator(object):
+    def __init__(self):
+        pass
+
+    def evaluate_rpn(self, rpn):
+        """Evaluate RPN"""
+        # TODO: add your code here
+        return 85
